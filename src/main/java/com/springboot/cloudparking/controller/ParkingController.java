5d5bdbaf8dd4e387d716961e5d4bfce217dbc41f
mateusrdn <mateusrn6@gmail.com>
2022-08-07 21:38:57 -0300

    Endpoint list

---
+++ src/main/java/com/springboot/cloudparking/controller/ParkingController.java
@@ -0,0 +1,34 @@
+package com.springboot.cloudparking.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springboot.cloudparking.controller.DTO.ParkingDTO;
+import com.springboot.cloudparking.controller.mapper.ParkingMapper;
+import com.springboot.cloudparking.model.Parking;
+import com.springboot.cloudparking.service.ParkingService;
+
+@RestController
+@RequestMapping("/parking")
+public class ParkingController {
+	
+	private final ParkingService parkingService;
+	private final ParkingMapper parkingMapper;
+	
+	public ParkingController(ParkingService parkingService, ParkingMapper parkingMapper) {
+		this.parkingService = parkingService;
+		this.parkingMapper = parkingMapper;
+	}
+
+	@GetMapping
+	public ResponseEntity<List<ParkingDTO>> findAll(){
+		List<Parking> parkingList = parkingService.findAll();
+		List<ParkingDTO> result = parkingMapper.toParkingDTOList(parkingList);
+		return ResponseEntity.ok(result);
+	}
+	
+}
---
+++ src/main/java/com/springboot/cloudparking/controller/mapper/ParkingMapper.java
@@ -0,0 +1,27 @@
+package com.springboot.cloudparking.controller.mapper;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import com.springboot.cloudparking.controller.DTO.ParkingDTO;
+import com.springboot.cloudparking.model.Parking;
+
+@Component
+public class ParkingMapper {
+
+	private static final ModelMapper MODEL_MAPPER = new ModelMapper();
+	
+	public ParkingDTO parkingDTO(Parking parking) {
+		return MODEL_MAPPER.map(parking, ParkingDTO.class);
+	}
+	
+	public List<ParkingDTO> toParkingDTOList(List<Parking> parkingList){
+		return parkingList.stream().map(this::parkingDTO).collect(Collectors.toList());
+	}
+	
+	
+	
+}
