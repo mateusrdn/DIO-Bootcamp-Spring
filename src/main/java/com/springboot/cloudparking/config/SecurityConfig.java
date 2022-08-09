fa920a60bc781723b695b0a90df141a3ec700443
Mateus Rodrigues do Nascimento <90523108+mateusrdn@users.noreply.github.com>
2022-08-09 16:20:59 -0300

    SpringSecurity

---
+++ src/main/java/com/springboot/cloudparking/config/SecurityConfig.java
@@ -0,0 +1,55 @@
+package com.springboot.cloudparking.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	//Configurar o login e usuario
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+		auth.inMemoryAuthentication()
+		.withUser("mateusrn")
+		.password(passwordEncoder().encode("virus"))
+		.roles("USER")
+		.and()
+		.passwordEncoder(passwordEncoder());
+	}
+	
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http.authorizeHttpRequests()
+		.antMatchers("/swagger-ui.html").permitAll()
+		.antMatchers("/swagger-ui.html").permitAll()
+		.antMatchers("/swagger-resources/**").permitAll()
+		.antMatchers("/webjars/**").permitAll()
+		.antMatchers("/v2/api-docs/**").permitAll()
+		.antMatchers("/").permitAll()
+		.antMatchers("/csrf").permitAll()
+		.antMatchers("/*.js").permitAll()
+		.antMatchers("/*.css").permitAll()
+		.antMatchers("/*.ico").permitAll()
+		.antMatchers("/*.png").permitAll()
+		.anyRequest().authenticated()
+		.and().httpBasic()
+		.and()
+		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		
+	}  
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+	
+	
+}
