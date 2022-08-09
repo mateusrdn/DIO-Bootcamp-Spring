ef52c40c5dd3b68af1c921a5b0ffe801747d2dfc
Mateus Rodrigues do Nascimento <90523108+mateusrdn@users.noreply.github.com>
2022-08-09 12:11:48 -0300

    CheckOut

--- src/main/java/com/springboot/cloudparking/controller/ParkingController.java
+++ src/main/java/com/springboot/cloudparking/controller/ParkingController.java
@@ -75,8 +75,8 @@ public class ParkingController {
 	}
 	
 	@PostMapping
-	public ResponseEntity<ParkingDTO> exit(@PathVariable String id){
-		Parking parking = parkingService.exit(id);
+	public ResponseEntity<ParkingDTO> chechOut(@PathVariable String id){
+		Parking parking = parkingService.checkOut(id);
 		return ResponseEntity.ok(parkingMapper.toParkingDTO(parking));
 	}
 	
